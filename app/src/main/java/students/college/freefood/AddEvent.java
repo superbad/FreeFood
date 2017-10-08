8813ac618c05d0eafdba2ed0e3dba1ba387fa6d4
Robert Bradshaw <arejaybee95@gmail.com>
2017-10-07 20:57:00 -0400

    Better working NavigationMenu!

---
+++ app/src/main/java/students/college/freefood/AddEvent.java
@@ -0,0 +1,22 @@
+package students.college.freefood;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by Robert Bradshaw on 10/7/2017.
+ */
+
+public class AddEvent extends Activity
+{
+    private FreeFoodEvent ffe;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+
+
+    }
+}
